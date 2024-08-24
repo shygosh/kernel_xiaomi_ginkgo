e1e8bc9b5072bda9d2c07833b25ea37acc59a21d
shygosh <shygosh@proton.me>
2024-08-24 19:50:29 +0700

    sched/fair: Check for sched_idle-only CPU in idle_cpu()
    Signed-off-by: shygosh <shygosh@proton.me>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4324,6 +4324,10 @@ int idle_cpu(int cpu)
 {
 	struct rq *rq = cpu_rq(cpu);
 
+	if (unlikely(rq->nr_running &&
+		rq->nr_running == rq->cfs.idle_h_nr_running))
+		return 1;
+
 	if (rq->curr != rq->idle)
 		return 0;
 
