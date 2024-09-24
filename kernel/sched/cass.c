2018dba716488c9cd91d4f066e00649fabb3f033
shygosh <shygosh@proton.me>
2024-09-25 02:26:03 +0700

    sched/cass: Don't consider selecting CPU marked as reserved
    A CPU marked as reserved is a target of active balance migration.
    
    Signed-off-by: shygosh <shygosh@proton.me>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -133,6 +133,9 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 		struct cpuidle_state *idle_state;
 		struct rq *rq = cpu_rq(cpu);
 
+		if (is_reserved(cpu))
+			continue;
+
 		/*
 		 * Check if this CPU is idle or only has SCHED_IDLE tasks. For
 		 * sync wakes, always treat the current CPU as idle.
